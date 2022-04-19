3e7da4ca197c6d7a96cce1d992068968a9fe3791
wyatharth <iplaybuldaboat@gmail.com>
2022-04-19 22:06:53 +0530

    UPDATED!!11!1!!!111!# Please enter the commit message for your changes. Lines starting

--- Triangler.py
+++ Triangler.py
@@ -12,9 +12,9 @@ Length = int(input(prompt))
 print("printing", Length, "rows")
 
 def triangle(Length):
-    for i in range(0, Length):
-        for j in range(0, i+1):
-            print("# ",end="") 
+    for abc in range(0, Length):
+        for jcb in range(0, abc+1):
+            print(" * ",end="") 
         print("\n")  
 
 triangle(Length)
