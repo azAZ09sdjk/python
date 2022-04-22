4744f073e16fae1711c70d9f14542efe44ba7e79
Amol Wankhede <awankhede@kd-4c4c4544-005a-4310-8058-b6c04f373633.ibm.com>
2022-04-22 07:01:01 +0530

    remove if statements before function.

--- Triangler.py
+++ Triangler.py
@@ -1,6 +1,4 @@
 
-
-
 prompt = "-->"
 
 print("Hello Welcome")
@@ -19,24 +17,25 @@ if TriType == "str":
 print("printing", Length, "rows")
 
 
-if TriType == "str" :
-    def triangleNor(Length):
-        for abc in range(0, Length):
-            for jcb in range(0, abc+1):
-                print(strtp, end=" ") 
-            print("\n")
+#if TriType == "str" :
 
-if TriType == "int":
-    def triangleNum(length):
-        for bcd in range(0, Length + 1):
-            for op in range(0, bcd):
-                print (bcd , end = " ")
-            print("\n")
+def triangleNor(Length):
+    for abc in range(0, Length):
+        for jcb in range(0, abc+1):
+            print(strtp, end=" ") 
+        print("\n")
+
+#if TriType == "int":
+def triangleNum(Length):
+    for bcd in range(0, Length + 1):
+        for op in range(0, bcd):
+            print (bcd , end = " ")
+        print("\n")
+
 
 if TriType == "str":
     triangleNor(Length)
 
 if TriType == "int":
     triangleNum(Length)
-
 
