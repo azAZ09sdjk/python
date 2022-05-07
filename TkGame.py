d534918e8741db3635fb8c995342226fa34f6dc9
wyatharth <iplaybuldaboat@gmail.com>
2022-05-08 00:19:05 +0530

    code Edited

--- TkGame.py
+++ TkGame.py
@@ -1,9 +1,37 @@
-from tkinter import messagebox
-from Tkinter import *
 import time
+from tkinter import messagebox
+from tkinter import *
+import threading
 
-money = 0
-Invests: int = 0
+money = 0.0
+Invests: float = 0
+Invests_money = 0.0
+time0 = 0
+stopInvest: bool = True
+count = 0
+countlocal = 0
+total_money = 0
+
+
+def timepas():
+    global time0
+    global money
+    global total_money
+    global Invests
+    global Invests_money
+    global stopInvest
+    global countlocal
+    while stopInvest:
+        countlocal = countlocal + 1
+        Invests_money = Invests * 2
+        money = money + Invests_money
+        print(money)
+        time.sleep(1)
+        if countlocal == count:
+            stopInvest = False
+        continue
+
+
 
 def work_money():
     global money
@@ -11,29 +39,58 @@ def work_money():
 
 
 def BankAcc():
-    messagebox.showinfo("Your Bank Account", "You have $:"+str(money))
+    global money
+    messagebox.showinfo("Your Bank Account", "You have $:" + str(total_money))
 
 
 def Inves():
-    global Invests 
-    Invests = Invests + 1
-    
+    global Invests
+    global money
+    if money >= 10:
+        Invests = Invests + 1
+        money = money - 10
+        print("Invested")
+        print("Now you have $:", money)
+        print("You have ", Invests, " Invests")
+        print()
+        timepas()
+    elif money < 10:
+        print("You don't have enough money.")
+        print("Tip:Press work to get more money")
 
 
+def stop_investing():
+    global count
+    global countlocal
+    global total_money
+    global money
+
+    count = 10
+    countlocal
+    total_money = total_money + money
+
+
+# Threads
+#p = threading.Thread(target=timepas)
+#p.start()
+
+# tkinter stuff
 gamesc = Tk()
 gamesc.title("Clickathon")
 gamesc.geometry("480x270")
 gamesc.iconbitmap('F:/GIT_REPO/python/python/TkGame.ico')
 
-work = Button(text="Do work", command=work_money)
-work.pack(side=TOP)
+work = Button(text="Work", command=work_money)
+work.pack(side=TOP, ipadx=20, ipady=20)
 
 CkBankAcc = Button(text="Check Bank Account", command=BankAcc)
 CkBankAcc.pack(side=BOTTOM)
 
-#i cant figure this out Help me
-if money > 10:
-    ShareMar = Button(text="Invest in Shares", command=Inves)
-    ShareMar.pack(side = LEFT)
+# I can't figure this out Help me
+
+ShareMar = Button(text="Invest in Shares", command=Inves)
+ShareMar.pack(side=LEFT)
+ShareMar1 = Button(text="Invest for 10 Years", command=stop_investing)
+ShareMar1.pack(side=RIGHT)
 
 gamesc.mainloop()
--- Tkinter.py
+++ Tkinter.py
@@ -1,32 +1,36 @@
 from tkinter import *
 
+
 def redbg():
     top.configure(bg='red')
+
 
 def blubg():
     top.configure(bg='blue')
 
+
 def grenbg():
     top.configure(bg='green')
 
+
 def blakbg():
     top.configure(bg='black')
+
 
 top = Tk()
 top.geometry("570x600")
 top.title("Colour changer")
 
-redbut = Button(top, text="Red", fg="red", font='Helvetica 14 bold', command=redbg,padx = 40, pady = 40)
+redbut = Button(top, text="Red", fg="red", font='Helvetica 14 bold', command=redbg, padx=40, pady=40)
 redbut.pack(side=RIGHT)
 
-blubut = Button(top, text="Blue", fg="blue", font='Helvetica 14 bold', command=blubg,padx = 40, pady = 40)
+blubut = Button(top, text="Blue", fg="blue", font='Helvetica 14 bold', command=blubg, padx=40, pady=40)
 blubut.pack(side=RIGHT)
 
-grenbut = Button(top, text="Green", fg="green", font='Helvetica 14 bold', command=grenbg,padx = 40, pady = 40)
+grenbut = Button(top, text="Green", fg="green", font='Helvetica 14 bold', command=grenbg, padx=40, pady=40)
 grenbut.pack(side=RIGHT)
 
-blakbut = Button(top, text="Black", fg="black", font='Helvetica 14 bold', command=blakbg,padx = 40, pady = 40)
+blakbut = Button(top, text="Black", fg="black", font='Helvetica 14 bold', command=blakbg, padx=40, pady=40)
 blakbut.pack(side=RIGHT)
 
 top.mainloop()
-
